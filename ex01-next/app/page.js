08ce708363a8e78311fdd7a6c6abb57d294ab2c9
Jonhnata Ferreira da Silva <111892785+Jonhnata@users.noreply.github.com>
2025-08-19 00:05:18 +0000

    s

--- ex01-next/app/page.js
+++ ex01-next/app/page.js
@@ -1,17 +1,12 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import { Profile } from "@/components/Profile";
 
 export default function Home() {
-return (
-
-  <section>
-    <h1>Amazing scientists</h1>
-    <Profile/>
-    <Profile/>
-    <Profile/>
-    <hr/>
-    <TodoList/>
-  </section>
-)
-   
+  return (
+    <div>
+      <h1>Amazing scientists</h1>
+      <Profile />
+      <Profile />
+      <Profile />
+    </div>
+  );
 }
